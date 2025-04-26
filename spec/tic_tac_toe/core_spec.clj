b3d184e737b7f24fcf6749241827fe39b425124d
Austin Cripe <austincripe@Austins-Mac-mini.local>
2025-04-25 23:03:04 -0400

    tie and winner functions

--- spec/tic_tac_toe/core_spec.clj
+++ spec/tic_tac_toe/core_spec.clj
@@ -3,6 +3,23 @@
             [tic-tac-toe.core :refer :all]))
 
 (describe "tic tac toe"
-  (it "FIXME, I fail."
-    (should= "test" (testinput)));
+  (with-stubs)
+
+  (redefs-around [read-line (stub :read-line {:return "X"})])
+
+  (it "reads from stdin"
+    (should= "X" (read-line)))
+
+  (it "tie game"
+    (should (tie-game? [["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["o"] ["x"]]))
+    (should-not (tie-game? [["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] [""] [""]])))
+
+  (it "someone made winning move"
+    (should (winner? [0 1 2]))
+    (should (winner? [1 4 7]))
+    )
+
+  (it "ai turn"
+    (should= [["o"] [""] [""] [""] [""] [""] [""] [""] [""]]
+      (ai-turn ( atom [[""] [""] [""] [""] [""] [""] [""] [""] [""]]))))
   )
--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,22 +1,30 @@
 (ns tic-tac-toe.core
   (:require [clojure.string :as str]))
 
-(def board ["\n"
-            [""] [""] [""] "\n"
-            [""] ["x"] [""] "\n"
-            ["x"] [""] [""] "\n"])
+(def board (atom [[""] [""] [""] [""] [""] [""] [""] [""] [""]]))
+
 (def winning-moves [[0 1 2] [3 4 5] [6 7 8]
                     [0 3 6] [1 4 7] [2 5 8]
                     [0 4 8] [2 4 6]])
 
-(defn testinput []
-  (let [nboard board]
-  (println nboard)
-  (flush)
-  (def x (read-line))
-  (str/replace (nth nboard (read-string x)) "" "x")
-  (flush)
-  (println nboard)))
+(defn winner? [coll]
+  (map #(contains? winning-moves %) coll))
 
+(defn tie-game? [b]
+  (every? false? (map #(empty? (first %)) b)))
 
+(defn ai-turn [brd]
+  (let [b brd
+        ;choice (rand-int (count (filter #(true? %) (map #(empty? (first %)) b))))
+        ]
+    (deref board))
+  )
+
+(defn init-game []
+  (let [b board x (read-line)]
+    ))
+
+(defn -main [& args]
+  (let [x (read-line)]
+    x))
 
