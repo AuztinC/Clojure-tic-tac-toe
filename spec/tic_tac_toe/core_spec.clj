e8df750de8c55d385ac07729a076c69ec15b39cb
Austin Cripe <austincripe@Austins-Mac-mini.local>
2025-04-27 16:27:38 -0400

    winning + tie checking update

--- spec/tic_tac_toe/core_spec.clj
+++ spec/tic_tac_toe/core_spec.clj
@@ -5,9 +5,9 @@
 (describe "tic tac toe"
   (with-stubs)
 
-  (redefs-around [read-line (stub :read-line {:return "X"})])
+  ;(redefs-around [read-line (stub :read-line {:return "X"})])
 
-  (it "init"
+  #_(it "init"
     (should= -1 (init-game)))
 
   (it "tie game"
@@ -15,11 +15,16 @@
     (should-not (tie-game? [["x"] ["x"] ["x"] ["x"] ["x"] ["x"] ["x"] [""] [""]])))
 
   (it "someone made winning move"
-    (should (winner? [0 1 2]))
-    (should (winner? [1 4 7]))
+    (should= "x" (winner? [["x"] ["x"] ["x"] ["o"] ["x"] ["x"] [""] [""] [""]]))
+    (should-not (winner? [["o"] ["x"] ["x"] ["o"] ["x"] ["x"] [""] [""] [""]]))
+    (should= "x" (winner? [["o"] ["x"] ["x"] ["o"] ["x"] ["x"] ["x"] [""] [""]]))
+    (should= "o" (winner? [["o"] ["x"] ["x"] ["o"] ["o"] ["x"] [""] [""] ["o"]]))
+    (should (winner? [["x"] ["x"] ["o"]
+                           ["o"] ["o"] ["x"]
+                           ["x"] ["o"] ["x"]]))
     )
 
-  #_(it "ai turn : currently returns random space"
+  #_(it "ai turn : currently returns index of random space"
     (should= -1
       (ai-turn [[""] [""] [""] [""] [""] [""] [""] [""] [""]])))
   )
--- src/tic_tac_toe/core.clj
+++ src/tic_tac_toe/core.clj
@@ -1,27 +1,48 @@
 (ns tic-tac-toe.core
   (:require [clojure.string :as str]))
 
-(def board  [[""] [""] [""] [""] [""] [""] [""] [""] [""]])
+(def board [[""] [""] [""] [""] [""] [""] [""] [""] [""]])
 
 (def winning-moves [[0 1 2] [3 4 5] [6 7 8]
                     [0 3 6] [1 4 7] [2 5 8]
                     [0 4 8] [2 4 6]])
 
-(defn winner? [coll]
-  (every? false? (map #(contains? winning-moves %) coll)))
 
 (defn tie-game? [b]
   (every? false? (map #(empty? (first %)) b)))
 
+(defn winner? [b]
+  (let [wm->board (for [wm winning-moves] (reduce (fn [acc x] (conj acc (nth b x))) [] wm))]
+    (let [winner-result (first (first (first
+                    (filter #(if (and (not= "" (first (first %))) (every? #{(first %)} %))
+                               (first %) false)
+                      wm->board))))]
+      (if winner-result
+        winner-result
+        (tie-game? b)))
+
+
+    #_(first (first (filter #(coll? %) (reduce
+                                         (fn [acc x]
+                                           (conj acc
+                                             (if (and (not= "" (first (first x))) (every? #{(first x)} x))
+                                               (first x)
+                                               false)))
+                                         [] wm->board)))))
+  )
+
+
+
 (defn ai-turn [b]
   (rand-int (count (filter #(true? %) (map #(empty? (first %)) b)))))
-;x (read-line)
+
 (defn init-game []
   (loop [b board turn "ai"]
     (println b)
-    (if (= "player" turn)
-      [(winner? b) (tie-game? b)]
-      (if (= "ai" turn)
+    (if (or (winner? b) (tie-game? b))
+      (println "game over")
+      (if (= "player" turn)
+        (recur (assoc b (Integer/parseInt (read-line)) ["X"]) "ai")
         (recur (assoc b (ai-turn b) ["O"]) "player")))))
 
 (defn -main [& args]
